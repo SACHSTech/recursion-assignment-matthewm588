06f969e289d062cccbfb0ae85a2148473d4e65ca
matthewm11 <matthew.morelli21@ycdsbk12.ca>
2020-12-04 23:19:07 +0000

    defined first test

--- src/recursionAssignment/RecursionTest.java
+++ src/recursionAssignment/RecursionTest.java
@@ -1,17 +1,17 @@
 package recursionAssignment;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Before;
-import org.junit.jupiter.api.After;
+//import org.junit.jupiter.api.Before;
+//import org.junit.jupiter.api.After;
 
 import recursionAssignment.Recursion;
 
 public class RecursionTest{
-    
+    /*
     @Before
     public void beforeTest(){
       // some steps to perform before tests are run
-    }
+    }*/
     
     /**
      * An initial test
@@ -21,14 +21,16 @@ public class RecursionTest{
         // make assertion statement(s)
         // assertEquals(EXPECTEDVALUE, TESTVALUE);
         // assertTrue(TESTBOOLEAN);
+        assertEquals(Recursion.countHi("xyhixy"));
     }
     
     // add more tests
-    
+
+    /*
     @After
     public void afterTest(){
       // some steps to perform after tests are run
-    }
+    }*/
     
     
 }
