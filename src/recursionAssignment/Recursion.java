4bcffa8e0c57b17bed4b8ea65167a44246b6cb58
matthewm11 <matthew.morelli21@ycdsbk12.ca>
2020-12-05 20:16:19 +0000

    refactored and added more tests (all for question 2)

--- src/recursionAssignment/Recursion.java
+++ src/recursionAssignment/Recursion.java
@@ -24,7 +24,17 @@ public class Recursion{
 
   public static String pairStar(String str) {
 
-    return "hel*lo";
+    if (str.length() == 1 || str.equals("")) {
+
+      return str;
+
+    } else if (str.substring(0, 1).equals(str.substring(1, 2))) {
+
+      return str.substring(0, 1) + "*" + pairStar(str.substring(1));
+
+    }
+
+    return str.substring(0, 1) + pairStar(str.substring(1));
 
   }
 
